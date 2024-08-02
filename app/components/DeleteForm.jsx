9b154b199e6feed7f962b77fb68ed14dd5221d69
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-08-01 23:46:24 -0400

    delete form

--- app/components/DeleteForm.jsx
+++ app/components/DeleteForm.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+"use client"
+import { deleteTask } from '@/utils/actions'
+import React, { useState } from 'react'
+const DeleteForm = ({ id }) => {
+  const [showModal, setShowModal] = useState(false)
 
-const DeleteForm = async ({task}) => {
-  const tasks = await prisma.task.findMany({
-    orderBy: { createAt: 'desc' },
-  })
+  const handleDelete = async (e) => {
+    e.preventDefault()
+    deleteTask(new FormData(e.target))
+    setShowModal(false)
+  }
+
+  const handleCancel = (e) => {
+    setShowModal(false)
+  }
+
   return (
-    <div>
-      <button className='btn  bg-red-600'>Delete</button>
-      {/* <button onClick={() => prisma.task.delete({ where: { id: task.id } })}> </button> */}
-    </div >
+    <>
+      <button className='btn btn-xs btn-error' onClick={() => setShowModal(true)}>Delete </button>
+      {showModal && (
+        <dialog open className='modal'>
+          <form method='dialog' onSubmit={handleDelete} className='modal-box'>
+            <h3 className='font-bold text-lg'>Confirm Deletion</h3>
+            <p>Are you sure you want to delete this task?</p>
+            <input type='hidden' name='id' value={id} />
+            <div className='modal-action'>
+              <button className='btn btn-md btn-info' onClick={handleCancel}>Cancel</button>
+              <button type='submit' className='btn btn-md btn-error'>Delete</button>
+            </div>
+          </form>
+        </dialog>
+      )}
+    </>
   )
 }
-
 export default DeleteForm
-
-// < button onClick = {() => prisma.task.delete({ where: { id: task.id } })}> </button >
