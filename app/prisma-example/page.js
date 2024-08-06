818625ec13526ad801cb2212add7a0f633197c2d
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-08-06 01:35:53 -0400

    removed middleware

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import prisma from '@/utils/db'
 
 const prismaHandlers = async () => {
--- middleware.js
+++
@@ -1,9 +0,0 @@
-// import { NextResponse } from "next/server";
-
-// export function middleware(req) {
-//     return NextResponse.redirect(new URL('https://www.google.com', req.url.pathname));
-// }
-
-// export const config = {
-//     matcher: ['/about/:path*', '/tasks/']
-// }
