7bd4bbb3aac36a28fd295b195ed905ec16ed9dba
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-08-06 01:28:38 -0400

    made tasks force dynamic

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import TaskList from '../components/TaskList'
 import TaskForm from '../components/TaskForm'
-
+export const dynamic = "force-dynamic"
 
 const Tasks = () => {
     return (
